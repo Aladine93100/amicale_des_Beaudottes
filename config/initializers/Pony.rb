f8de43b6f0e0789840b52b0588875c4a98cef86f
Aladine <alazaiane@yahoo.fr>
2014-04-24 16:00:32 +0200

    modification du mail destination

--- config/initializers/Pony.rb
+++ config/initializers/Pony.rb
@@ -1,12 +1,12 @@
 Pony.options = {
-  :to => 'wwqp93@gmail.com',
+  :to => 'amicalebeaudottes@gmail.com',
   :via => :smtp,
   :via_options => {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
-    :user_name            => 'wwqp93',
-    :password             => 'tounsi93',
+    :user_name            => 'amicalebeaudottes',
+    :password             => 'amicalebdt270',
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
   }
