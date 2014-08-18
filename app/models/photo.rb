08f86048dcabdab6b4425020a6c66bf413182b42
Aladine <alazaiane@yahoo.fr>
2014-08-18 20:45:37 +0200

    modif photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,3 @@ class Photo < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates :avatar, :attachment_presence => true
 end
-end
