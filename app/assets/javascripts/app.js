1e4ab9740595473f031975011f8b71aec3e11dde
Aladine <alazaiane@yahoo.fr>
2014-08-27 21:00:32 +0200

    css et js menu

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -48,28 +48,28 @@ $('document').ready(function() {
 
 	$( '.coll1' ).click(function() {
 		$( '#coll1' ).show( 'slow' )
-		$( '#coll2, #coll3, #coll4, #coll5, .questionnaire2, .menu2' ).hide( 'slow' )
+		$( '#coll2, #coll3, #coll4, #coll5, .questionnaire2' ).hide( 'slow' )
 	});
 
 	$( '.coll2' ).click(function() {
 		$( '#coll2' ).show( 'slow' )
-		$( '#coll1, #coll3, #coll4, #coll5, .questionnaire2, .menu2' ).hide( 'slow' )
+		$( '#coll1, #coll3, #coll4, #coll5, .questionnaire2' ).hide( 'slow' )
 	});
 
 	$( '.coll3' ).click(function() {
 		$( '#coll3' ).show( 'slow' )
-		$( '#coll2, #coll1, #coll4, #coll5, .questionnaire2, .menu2' ).hide( 'slow' )
+		$( '#coll2, #coll1, #coll4, #coll5, .questionnaire2' ).hide( 'slow' )
 	});
 
 
 	$( '.coll4' ).click(function() {
 		$( '#coll4' ).show( 'slow' )
-		$( '#coll2, #coll3, #coll1, #coll5, .questionnaire2, .menu2' ).hide( 'slow' )
+		$( '#coll2, #coll3, #coll1, #coll5, .questionnaire2' ).hide( 'slow' )
 	});
 
 	$( '.coll5' ).click(function() {
 		$( '#coll5' ).show( 'slow' )
-		$( '#coll2, #coll3, #coll1, #coll4, .questionnaire2, .menu2' ).hide( 'slow' )
+		$( '#coll2, #coll3, #coll1, #coll4, .questionnaire2' ).hide( 'slow' )
 	});
 
 	$( '.menu3' ).click(function() {
@@ -79,27 +79,27 @@ $('document').ready(function() {
 
 	$( '.immeuble' ).click(function() {
 		$( '#immeuble' ).show( 'slow' )
-		$( '#poubelle, #enfant, #escal-pal, #hall, .menu3').hide( 'slow' )
+		$( '.poubelle, .enfant, .escal-pal, .hall').hide( 'slow' )
 	});
 
     $( '.poubelle' ).click(function() {
 		$( '#poubelle' ).show( 'slow' )
-		$( '#immeuble, #enfant, #escal-pal, #hall, .menu3').hide( 'slow' )
+		$( '.immeuble, .enfant, .escal-pal, .hall').hide( 'slow' )
 	});
 
     $( '.enfant' ).click(function() {
 		$( '#enfant' ).show( 'slow' )
-		$( '#poubelle, #immeuble, #escal-pal, #hall, .menu3').hide( 'slow' )
+		$( '.poubelle, .immeuble, .escal-pal, .hall').hide( 'slow' )
 	});
 
     $( '.escal-pal' ).click(function() {
 		$( '#escal-pal' ).show( 'slow' )
-		$( '#poubelle, #enfant, #immeuble, #hall, .menu3').hide( 'slow' )
+		$( '.poubelle, .enfant, .immeuble, .hall').hide( 'slow' )
 	});
 
     $( '.hall' ).click(function() {
 		$( '#hall' ).show( 'slow' )
-		$( '#poubelle, #enfant, #escal-pal, #immeuble, .menu3').hide( 'slow' )
+		$( '.poubelle, .enfant, .escal-pal, .immeuble').hide( 'slow' )
 	});
 
 
